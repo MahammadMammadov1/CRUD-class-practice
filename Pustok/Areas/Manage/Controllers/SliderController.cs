526e7961b649bedcf438d76a1da212110c814cae
sahincode <ismayilovsahin714@gmail.com>
2023-11-21 19:16:57 +0400

    task

---
+++ Pustok/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pustok.DAL;
+using Pustok.Models;
+
+namespace Pustok.Areas.Manage.Controllers
+{ 
+    [Area("Manage")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _slider;
+
+        public SliderController(AppDbContext appDb)
+        {
+            _slider = appDb;    
+        }
+        public IActionResult Index()
+        {
+            List<Slider> sliderList = _slider.Sliders.ToList();
+            return View(sliderList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Slider slider)
+        {
+            _slider.Sliders.Add(slider);
+            _slider.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var wanted = _slider.Sliders.FirstOrDefault(x => x.Id ==id);
+
+            return View(wanted);
+        }
+        [HttpPost]
+        public IActionResult Update(Slider slider)
+        {
+            var wanted = _slider.Sliders.FirstOrDefault(x => x.Id == slider.Id) ;
+
+            wanted.Title = slider.Title;
+            wanted.Description = slider.Description;
+            wanted.RedirctText = slider.RedirctText;
+            wanted.RedirctUrl = slider.RedirctUrl;
+            wanted.ImageUrl = slider.ImageUrl;
+
+            _slider.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id) 
+        {
+            var wanted = _slider.Sliders.FirstOrDefault(x => x.Id == id);
+
+            return View(wanted);
+        }
+        [HttpPost]
+        public IActionResult Delete(Slider slider)
+        {
+            var wanted = _slider.Sliders.FirstOrDefault(x => x.Id == slider.Id);
+            _slider.Sliders.Remove(wanted);
+            _slider.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Pustok/Models/Slider.cs
@@ -0,0 +1,14 @@
+﻿namespace Pustok.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+        public string RedirctUrl { get; set; }
+        public string RedirctText { get; set; }
+
+    }
+}
